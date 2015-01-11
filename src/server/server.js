5ca65ea473d21db10c472d21994fcd6628787604
Jennifer Parker <jenparker1975@gmail.com>
2015-01-11 14:48:58 -0500

    initial app commit

---
+++ src/server/server.js
@@ -0,0 +1,33 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var port = process.env.PORT || 8001;
+var path = require('path');
+
+var environment = process.env.NODE_ENV;
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.use(express.static('./src/client/'));
+app.use(express.static('./'));
+path.resolve(__dirname+'.../client/index.html');
+
+// Application routes
+app.get('*', function(req, res){
+	res.sendfile('../client/index.html', {'root': '../client/'});
+});
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+    console.log('env = ' + app.get('env') +
+        '\n__dirname = ' + __dirname  +
+        '\nprocess.cwd = ' + process.cwd());
+});
