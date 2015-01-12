2c618c105e41dfdc9e65558e7676ca8ce8bf3da5
Jennifer Parker <jenparker1975@gmail.com>
2015-01-11 19:45:54 -0500

    trying to fix favicon

--- src/server/server.js
+++ src/server/server.js
@@ -4,14 +4,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var port = process.env.PORT || 8001;
 var path = require('path');
 
 var environment = process.env.NODE_ENV;
 
-//app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(__dirname + '/pg.ico'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
