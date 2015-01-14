358d8941af85eaefe06a65c2c6b5c98107ea7517
Pierre Thelusma <pthelusma@gmail.com>
2015-01-14 13:42:26 -0500

    More missing commits

---
+++ src/server/routes/users.js
@@ -0,0 +1,27 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server, Db = mongo.Db, BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('FortyFivesDB', server);
+
+db.open(function(err, db) {
+if(!err) {
+console.log("Connected to 'FortyFivesDB' database");
+db.collection('Users', {strict:true}, function(err, collection) {
+if (err) {
+console.log("The 'users' collection doesn't exist...");
+}
+});
+}
+});
+
+
+
+exports.findAll = function(req, res) {
+db.collection('Users', function(err, collection) {
+collection.find().toArray(function(err, items) {
+res.send(items);
+});
+});
+};
